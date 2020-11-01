7657e52aad7796f35f819ada5762eab2fb1ccecf
Naoto Kaneko <naoty.k@gmail.com>
2020-11-01 23:53:10 +0900

    Add -h and --help option to show help

--- lib/blog/cli.rb
+++ lib/blog/cli.rb
@@ -1,10 +1,27 @@
 module Blog
   # Blog CLI
   class CLI
+    HELP_MESSAGE = <<~TEXT.freeze
+      Usage:
+        blog -h | --help
+      
+      Options:
+        -h --help  Show this message
+    TEXT
+    private_constant :HELP_MESSAGE
+
     def initialize
     end
 
-    def run
+    # Run +blog+ command with arguments
+    #
+    # @param [ARGV, Array<String>] arguments the arguments to +blog+ command.
+    def run(arguments: ARGV)
+      if arguments.include?("-h") || arguments.include?("--help")
+        puts HELP_MESSAGE
+        exit
+      end
+
       raise NotImplementedError
     end
   end
