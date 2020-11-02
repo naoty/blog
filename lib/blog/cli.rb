9f8f67b15a8461a2ff6ac2adafe9ea703b467472
Naoto Kaneko <naoty.k@gmail.com>
2020-11-02 19:01:26 +0900

    Test that CLI#run puts help message

--- lib/blog/cli.rb
+++ lib/blog/cli.rb
@@ -4,25 +4,35 @@ module Blog
     HELP_MESSAGE = <<~TEXT.freeze
       Usage:
         blog -h | --help
-      
+
       Options:
         -h --help  Show this message
     TEXT
     private_constant :HELP_MESSAGE
 
-    def initialize
+    # @param [ARGV, Array<String>] arguments the arguments to +blog+ command.
+    # @param [IO] output output stream for messages
+    # @param [IO] error_output output stream for error messages
+    def initialize(arguments: ARGV, output: $stdout, error_output: $stderr)
+      @arguments = arguments
+      @output = output
+      @error_output = error_output
     end
 
     # Run +blog+ command with arguments
-    #
-    # @param [ARGV, Array<String>] arguments the arguments to +blog+ command.
-    def run(arguments: ARGV)
-      if arguments.include?("-h") || arguments.include?("--help")
-        puts HELP_MESSAGE
+    def run
+      if help_needed?
+        @output.puts HELP_MESSAGE
         exit
       end
 
       raise NotImplementedError
+    end
+
+    private
+
+    def help_needed?
+      @arguments.include?("-h") || @arguments.include?("--help")
     end
   end
 end
---
+++ test/blog/test_cli.rb
@@ -0,0 +1,20 @@
+require "minitest/autorun"
+require "blog/cli"
+
+module Blog
+  class TestCLI < Minitest::Test
+    def test_help_option
+      output = StringIO.new
+      cli = CLI.new(arguments: ["-h"], output: output)
+
+      assert_raises(SystemExit) { cli.run }
+      assert_equal <<~TEXT, output.string
+        Usage:
+          blog -h | --help
+
+        Options:
+          -h --help  Show this message
+      TEXT
+    end
+  end
+end
