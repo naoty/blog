24e2458acebff06fa3af90ca19ae2ee5cf2780f2
Naoto Kaneko <naoty.k@gmail.com>
2020-11-07 14:54:40 +0900

    Build index page to public

--- lib/blog/command/build.rb
+++ lib/blog/command/build.rb
@@ -1,3 +1,5 @@
+require 'pathname'
+
 module Blog
   module Command
     # +blog build+ command
@@ -5,10 +7,25 @@ module Blog
       # @param [Pathname] source the directory Post data are persisted
       def initialize(source:)
         @post_repository = PostRepository.new(source: source)
+        @posts_renderer = PostsRenderer.new
       end
 
       # Run +blog build+ command
-      def run; end
+      def run
+        posts = @post_repository.all_posts_sorted_by_time
+        html = @posts_renderer.render(posts)
+        public_path = ensure_public_path
+        public_path.join('index.html').open('wb') { |file| file.puts html }
+      end
+
+      private
+
+      # @return [Pathname] public path that built files are output
+      def ensure_public_path
+        public_path = Pathname.pwd.join('public')
+        public_path.mkdir unless public_path.exist?
+        public_path
+      end
     end
   end
 end
---
+++ test/blog/command/test_build.rb
@@ -0,0 +1,72 @@
+require 'test_helper'
+require 'tmpdir'
+require 'nokogiri'
+
+module Blog
+  module Command
+    class TestBuild < Minitest::Test
+      POST_NUMBER = 2
+
+      def setup
+        @tmpdir = Pathname.new(Dir.mktmpdir)
+        source = setup_source(root: @tmpdir)
+        @command = Command::Build.new(source: source)
+      end
+
+      def teardown
+        @tmpdir.rmtree
+      end
+
+      # @param [Pathname] root the root path which includes source path
+      # @return [Pathname] source path
+      def setup_source(root:)
+        source = root.join('source')
+        source.mkdir
+        1.upto(POST_NUMBER) { |id| setup_post_dir(source: source, id: id) }
+        source
+      end
+
+      # @param [Pathname] source the source path which includes post directories
+      # @param [Integer] id the ID of post
+      def setup_post_dir(source:, id:)
+        post_dir = source.join(id.to_s)
+        post_dir.mkdir
+
+        post_path = post_dir.join('post.md')
+        post_path.open('wb') do |file|
+          file.puts dummy_post_content(id: id)
+        end
+      end
+
+      # @param [Integer] id the ID of post
+      def dummy_post_content(id:)
+        time = Time.new(2020, 1, 1) + (id - 1) * 24 * 60 * 60
+        <<~TEXT
+          ---
+          title: dummy #{id}
+          time: #{time.strftime('%Y-%m-%d %H:%M')}
+          tags: ['ruby']
+          ---
+
+          # header
+          Here is paragraph.
+        TEXT
+      end
+
+      def test_run
+        Dir.chdir @tmpdir do
+          @command.run
+
+          public_path = @tmpdir.join('public')
+          assert public_path.exist?
+
+          index_path = public_path.join('index.html')
+          assert index_path.exist?
+
+          document = Nokogiri::HTML(index_path.read)
+          assert_equal POST_NUMBER, document.search('li').length
+        end
+      end
+    end
+  end
+end
