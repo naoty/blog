341f6a2a54bfb42efaf4282440323a4ada3bbba2
Naoto Kaneko <naoty.k@gmail.com>
2020-11-20 16:51:11 +0900

    Test Rack::PrettyURLs

---
+++ test/blog/rack/test_pretty_urls.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+module Blog
+  module Rack
+    class TestPrettyURLs < Minitest::Test
+      attr_reader :app
+
+      def setup
+        @app = PrettyURLs.new(->(env) { [200, env, '200 OK'] })
+      end
+
+      def test_call_with_trailing_slash
+        request = ::Rack::MockRequest.env_for('/')
+        _, env, _ = app.call(request)
+        assert_equal env[::Rack::PATH_INFO], '/index.html'
+      end
+
+      def test_call_without_trailing_slash
+        request = ::Rack::MockRequest.env_for('/test')
+        _, env, _ = app.call(request)
+        assert_equal env[::Rack::PATH_INFO], '/test'
+      end
+    end
+  end
+end
