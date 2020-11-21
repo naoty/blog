b7397c4b32c338a4ee2d17d8d793de4db41d86bc
Naoto Kaneko <naoty.k@gmail.com>
2020-11-21 11:56:49 +0900

    Refactor Blog::Command::Serve

--- lib/blog/command/serve.rb
+++ lib/blog/command/serve.rb
@@ -4,17 +4,11 @@ module Blog
   module Command
     # +blog serve+ command
     class Serve
+      attr_reader :source
+
       # @param [Pathname] source the source of Posts
       def initialize(source:)
-        base_app = ::Rack::Files.new(Blog.public_path)
-
-        @rack_app = ::Rack::Builder.new do
-          use Rack::NotFound
-          use Rack::PostsBuild, path: '/', source: source
-          use Rack::PostBuild, path: %r{/(?<id>\d+)/}, source: source
-          use Rack::PrettyURLs
-          run base_app
-        end
+        @source = source
       end
 
       def run
@@ -31,7 +25,21 @@ module Blog
       end
 
       def start_server
-        ::Rack::Handler::WEBrick.run(@rack_app)
+        ::Rack::Handler::WEBrick.run(rack_app)
+      end
+
+      def rack_app
+        @rack_app ||= ::Rack::Builder.new do
+          use Rack::NotFound
+          use Rack::PostsBuild, path: '/', source: source
+          use Rack::PostBuild, path: %r{/(?<id>\d+)/}, source: source
+          use Rack::PrettyURLs
+          run base_app
+        end
+      end
+
+      def base_app
+        @base_app ||= ::Rack::Files.new(Blog.public_path)
       end
     end
   end
