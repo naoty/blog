aafc24abbe17ebdb5a88e6b636ffca72f178f8cb
Naoto Kaneko <naoty.k@gmail.com>
2020-11-21 12:00:39 +0900

    Refactor Blog::PostsRenderer

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,4 +1,5 @@
 require 'bundler/setup'
+require 'erb'
 require 'pathname'
 
 # Top-level namespace
--- lib/blog/posts_renderer.rb
+++ lib/blog/posts_renderer.rb
@@ -1,18 +1,21 @@
-require 'erb'
-
 module Blog
   # Renderer for index page of posts
   class PostsRenderer
-    def initialize
-      template = Blog.root_path.join('template', 'index.html.erb')
-      @erb = ERB.new(template.read, trim_mode: '-')
-    end
-
     # Render HTML from posts
     # @param [Array<Post>] posts the list of posts
     # @return [String] rendered HTML
     def render(posts)
-      @erb.result(binding)
+      erb.result(binding)
+    end
+
+    private
+
+    def erb
+      @erb ||= ERB.new(template.read, trim_mode: '-')
+    end
+
+    def template
+      @template ||= Blog.root_path.join('template', 'index.html.erb')
     end
   end
 end
